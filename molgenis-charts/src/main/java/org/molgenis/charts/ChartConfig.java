a02d23774dda13824f11ebedd56a93d3529620c4
erwinwinder <j.a.winder@umcg.nl>
2013-10-10 12:22:45 +0200

    Removed hardcoded path

--- molgenis-charts/src/main/java/org/molgenis/charts/ChartConfig.java
+++ molgenis-charts/src/main/java/org/molgenis/charts/ChartConfig.java
@@ -1,21 +1,31 @@
 package org.molgenis.charts;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.molgenis.data.DataService;
 import org.molgenis.data.excel.ExcelEntitySourceFactory;
 import org.molgenis.data.support.DataServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.FileCopyUtils;
 
 @Configuration
 public class ChartConfig
 {
 	@Bean
-	public DataService dataService()
+	public DataService dataService() throws IOException
 	{
+		InputStream in = getClass().getResourceAsStream("/weather.xls");
+		File xlsFile = File.createTempFile("molgenis", ".xls");
+		FileCopyUtils.copy(in, new FileOutputStream(xlsFile));
+		String url = "excel://" + xlsFile.getAbsolutePath();
+
 		DataService dataService = new DataServiceImpl();
 		dataService.registerFactory(new ExcelEntitySourceFactory());
-		dataService
-				.registerEntitySource("excel:///Users/erwin/projects/molgenis/molgenis-charts/src/main/resources/weather.xls");
+		dataService.registerEntitySource(url);
 
 		return dataService;
 	}
